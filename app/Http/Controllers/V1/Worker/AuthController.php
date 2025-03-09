dd01eca49dae5f7f3b5701f32c8c75faa04ee107
javniktool <nikitakremnev0@gmail.com>
2025-03-09 15:39:22 +0300

    final

--- app/Http/Controllers/V1/Worker/AuthController.php
+++ app/Http/Controllers/V1/Worker/AuthController.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     /**
-     * user authentication
+     * worker authentication
      * @response array{ message: "success", token: string }
      * @throws RecordNotFoundException
      * @throws TokenNotCreatedException|RecordNotFoundException
